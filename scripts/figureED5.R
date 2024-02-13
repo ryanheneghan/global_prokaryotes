2845d703f42a5f033b5a231b8bc2b75d28ed10ab
Ryan heneghan <53048532+ryanheneghan@users.noreply.github.com>
2024-02-13 13:03:16 +1000

    Add files via upload

--- scripts/figureED5.R
+++ scripts/figureED5.R
@@ -8,11 +8,7 @@ library(data.table)
 source('./scripts/plot_map_func.R')
 
 ## Import data
-all_data <- data.table::fread("./data/contemporary_bacteria.csv")
-lonlat <- expand.grid("Long" = -179.5:179.5, "Lat" = -89.5:89.5)
-
-chl_data <- all_data %>% dplyr::filter(logdepth == min(logdepth)) %>% dplyr::select(Long, Lat, logchlo) %>%
-              left_join(lonlat, ., by = c("Long" = "Long", "Lat" = "Lat"))
+chl_data <- read.csv("./data/surface_chl.csv")
 
 plot_chl <- map_figure(chl_data, curr_pal = "Emrld", legend_name = "", plot_lims = c(-1.5, 0.69897), unlog_leg = TRUE, dp = 2, var_name = "")  + 
   theme(plot.title = element_text(face = "bold", size = 8, vjust = 1, margin = unit(c(0.1,0,0,0), unit = "cm")),
